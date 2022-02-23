f85c1770c254bbccbee884811fa6667be46f9326
Tomasz Dłuski <mr.toumash@gmail.com>
2022-02-23 20:29:13 +0100

    add test project

---
+++ Slack.RoundRobinAssignments.Tests/Extensions/DateExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Shouldly;
+using Slack.RoundRobinAssignments.Extensions;
+using Xunit;
+
+namespace Slack.RoundRobinAssignments.Tests.Extensions;
+
+public class DateExtensionsTests
+{
+    [Fact]
+    public void GetNextWorkingDay_GivenMonday_ShouldReturn_Tuesday()
+    {
+        DateExtensions.GetNextWorkingDay(new DateTime(2022, 01, 31)).Date.ShouldBe(new DateTime(2022, 02, 01));
+    }
+    
+    [Fact]
+    public void GetNextWorkingDay_GivenFriday_ShouldReturn_Monday()
+    {
+        DateExtensions.GetNextWorkingDay(new DateTime(2022, 02, 4)).Date.ShouldBe(new DateTime(2022, 02, 07));
+    }
+}
